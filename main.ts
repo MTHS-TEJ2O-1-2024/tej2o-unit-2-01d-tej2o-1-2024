876d677a2b2e15ee9a1fcc9c2f5bebc1e482b162
tej2o-1-2024 <tej2o-1-2024@mths.ca>
2024-09-10 10:28:29 -0400

    added correct comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
  * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created on: Sep 2024
+ * This program says Hello World
 */
 
 basic.showString('Hello, World!')
